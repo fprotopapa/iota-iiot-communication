e8eef633aeb062a167d468ce8b9e6a6c23009f7c
Fabbio Protopapa <fprotopapa.git@gmail.com>
2022-05-02 19:51:38 +0200

    Docker for Client (Factory)

--- docker/client_factory.sh
+++ docker/client_factory.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-
-cd ./rust-mqtt-service && ./target/debug/mqtt-server &
-
-cd ./iota-identity-service && ./target/debug/iota-identity-server &
-
-cd ./iota-streams-service && ./target/debug/iota-streams-server &
-
-cd ./client && RUST_LOG=info ./target/debug/gateway
-
-
-# ./mqtt-server &
-# ./iota-identity-server &
-# ./iota-streams-server &
-# ( sleep 10 ; ./mock-client ) &
-# RUST_LOG=info ./gateway
+./mqtt-server &
+./iota-identity-server &
+./iota-streams-server &
+./client
