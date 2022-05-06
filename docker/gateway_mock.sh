8c8e1464d34e2338170104520a2e9ba21d8004bf
Fabbio Protopapa <fprotopapa.git@gmail.com>
2022-05-06 19:00:13 +0200

    Update Dockerfiles

--- docker/gateway_mock.sh
+++ docker/gateway_mock.sh
@@ -3,5 +3,9 @@
 ./mqtt-server &
 ./iota-identity-server &
 ./iota-streams-server &
-( sleep 120 ; ./mock-client ) &
+
+FILE=./storage/database.db
+test -e "$FILE" && echo 'Restart ...' && ( sleep 10 ; ./mock-client ) &
+! test -e "$FILE" && echo 'First Startup ...' && ( sleep 120 ; ./mock-client ) &
+
 RUST_LOG=info ./gateway &
--- docker/gateway_opcua.sh
+++ docker/gateway_opcua.sh
@@ -4,5 +4,9 @@
 ./iota-identity-server &
 ./iota-streams-server &
 ./opcua-server &
-( sleep 50 ; ./opcua-client ) &
+
+FILE=./storage/database.db
+test -e "$FILE" && echo 'Restart ...' && ( sleep 10 ; ./opcua-client ) &
+! test -e "$FILE" && echo 'First Startup ...' && ( sleep 120 ; ./opcua-client ) &
+
 RUST_LOG=info ./gateway &
